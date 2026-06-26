24c38b2d80162e6766c0a7dc26bf1b07d088f0ab
Adam Heiler <heileradamm@gmail.com>
2026-06-26 16:42:18 -0400

    initial project setup with 6 page structure and working coolprop wasm integration git branch -M main git push -u origin main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import tailwindcss from '@tailwindcss/vite';
+import wasm from 'vite-plugin-wasm';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte()],
+  vite: {
+    plugins: [tailwindcss(), wasm()],
+  },
+});
